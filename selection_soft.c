d37e80cbb3e624eb4cc79f966ff7ade9144a44c5
duong.vh2414077 <duongvh.206@gmail.com>
2025-10-24 12:21:42 +0700

    selection_soft.c

---
+++ selection_soft.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+void printarray(int a[],int n)
+{
+    for (int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+int main()
+{
+    int n;
+    printf("n= ");
+    scanf("%d",&n);
+    int a[n];
+    
+    for (int i=0;i<n;i++)
+    {
+        printf("a[%d]= ",i+1);
+        scanf("%d",&a[i]);
+    }
+    int k=sizeof(a)/sizeof(a[0]);
+    //printarray(a,k);
+    int min;
+    for(int i=0;i<n-1;i++)
+    {
+        min=i;
+        for(int j=i+1;j<n;j++)
+        {
+        if(a[j]<a[min])
+        {
+            min=j;
+            int temp=a[min];
+            a[min]=a[j];
+            a[j]=temp;
+            printarray(a,k);
+            printf("\n");
+            
+        }
+        }
+    }
+    return 0;
+}
